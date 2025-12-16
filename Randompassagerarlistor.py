e5808a6f19b89bb6a4d139e730c8b5238cea56fd
EMIL <emil.ahman@skola.taby.se>
2025-12-16 12:10:58 +0100

    klar

---
+++ Randompassagerarlistor.py
@@ -0,0 +1,16 @@
+Förnamn = ["Emil ","Vilgot ","Frej ","Martin ","Lelle ","Peter ","Johannes ","Nikodemus ","Anna ","Ellie ","Maria ","Kenneth ","Pher ","Erland ","Leopold ","Mimmie ","Ann-Catrin ",]
+Efternamn = ["Åhman","Åman","Sundin","Hedenhös","Svedenmärkt","Thésen","Peterberg","Ahmmoudsson","Trump","Gatenmalm","Jansson","Bergling",""]
+Catchphrase =["Rör mig igen kommer jag att klubba ner dig!",
+    "Det där var min lever, tack för påminnelsen om att jag behöver donera den snart.",
+    "Petar du igen lär det bli det sista du gör.",
+    "Åh nej, nu väckte du parasiterna... de blir hungriga när man tar på mig.",
+    "Grattis, du vann precis galet mycket pengar, skriv in dina bankuppgifter bara.",
+    "Sluta! Jag är inte en knapp, jag är en mänsklig landmina!",
+    "Du anar inte hur nära du var att trycka på 'släpp lös tarminnehållet'-knappen.",
+    "Du är ju helt dum i huvudet.",
+    "Petar du en gång till så lovar jag att hosta upp något som har eget personnummer.",
+    "Bra jobbat, nu har du väckt min inre demon... och den är jävligt kittlig.",
+    "梨是健康的."
+]
+Busighet = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
