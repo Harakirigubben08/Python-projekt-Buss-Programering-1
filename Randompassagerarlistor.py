33034c146a6d8d8030ed6dd1bf1eccf1bfbf0be0
EMIL <emil.ahman@skola.taby.se>
2026-01-09 11:31:07 +0100

    catchphrase saker

--- Randompassagerarlistor.py
+++ Randompassagerarlistor.py
@@ -4,12 +4,12 @@ Catchphrase =["Rör mig igen kommer jag att klubba ner dig!",
     "Det där var min lever, tack för påminnelsen om att jag behöver donera den snart.",
     "Petar du igen lär det bli det sista du gör.",
     "Åh nej, nu väckte du parasiterna... de blir hungriga när man tar på mig.",
-    "Grattis, du vann precis galet mycket pengar, skriv in dina bankuppgifter bara.",
+    "Grattis, du vann precis galet mycket pengar, skriv bara in dina bankuppgifter.",
     "Sluta! Jag är inte en knapp, jag är en mänsklig landmina!",
     "Du anar inte hur nära du var att trycka på 'släpp lös tarminnehållet'-knappen.",
     "Du är ju helt dum i huvudet.",
-    "Petar du en gång till så lovar jag att hosta upp något som har eget personnummer.",
-    "Bra jobbat, nu har du väckt min inre demon... och den är jävligt kittlig.",
+    "Petar du en gång till så lovar jag att hosta upp något som har ett eget personnummer.",
+    "Hurru du din lurkiga lurk.",
     "梨是健康的."
 ]
 Busighet = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
